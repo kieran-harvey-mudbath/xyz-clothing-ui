e4e4cff8fb8a0ab2ac98583114321714bcdeb3f7
kieran.harvey@mudbath.com.au <kieran.harvey@mudbath.com.au>
2022-01-27 10:31:57 +1100

    scafold app

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ListComponent } from './views/list/list.component';
+import { DetailComponent } from './views/detail/detail.component';
+import { RelatedProductsComponent } from './components/related-products/related-products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListComponent,
+    DetailComponent,
+    RelatedProductsComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrencyPickerComponent } from './currency-picker/currency-picker.component';
+
+
+
+@NgModule({
+  declarations: [
+    CurrencyPickerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
